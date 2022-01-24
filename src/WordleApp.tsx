26a14828ee03e39053d404bd07c7b488748c077c
Marlon Gómez <marlonjulian15@gmail.com>
2022-01-24 11:41:01 -0500

    Import RowEmpty component

--- src/WordleApp.tsx
+++ src/WordleApp.tsx
@@ -1,9 +1,15 @@
 import { RowCompleted } from "./components/Rows/RowCompleted";
+import { RowEmpty } from "./components/Rows/RowEmpty";
 
 export default function WordleApp() {
   return (
     <div>
       <RowCompleted word="sabio" solution="break" />
+      <RowCompleted word="sabio" solution="break" />
+      <RowCompleted word="sabio" solution="break" />
+      <RowEmpty />
+      <RowEmpty />
+      <RowEmpty />
     </div>
   );
 }
