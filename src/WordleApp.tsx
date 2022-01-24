30dcd136a834444ed4fa073fbded38bcd9a163c6
Marlon Gómez <marlonjulian15@gmail.com>
2022-01-24 11:16:32 -0500

    Import Box component in WordleApp

--- src/WordleApp.tsx
+++ src/WordleApp.tsx
@@ -1,3 +1,10 @@
+import { Box } from "./components/Box/Box";
+
 export default function WordleApp() {
-  return <h1>Hola</h1>;
+  return (
+    <div>
+      <Box value="a" status="present" />
+      <Box value="hola" status="absent" />
+    </div>
+  );
 }
