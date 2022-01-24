fd3f8bd87d54e249d92b12bf04d4a139970def03
Marlon Gómez <marlonjulian15@gmail.com>
2022-01-24 11:52:57 -0500

    Create multiple status game

--- src/WordleApp.tsx
+++ src/WordleApp.tsx
@@ -1,12 +1,23 @@
+import { useEffect, useState } from "react";
 import { RowCompleted } from "./components/Rows/RowCompleted";
 import { RowCurrent } from "./components/Rows/RowCurrent";
 import { RowEmpty } from "./components/Rows/RowEmpty";
+import { GameStatus } from "./types/types";
 
 export default function WordleApp() {
+  const [wordOfTheDay, setWordOfTheDay] = useState<string>("");
+  const [turn, setTurn] = useState<number>(1);
+  const [currentWord, setCurrentWord] = useState<string>("");
+  const [completedWords, setCompletedWords] = useState<string[]>([]);
+  const [gameStatus, setGameStatus] = useState<GameStatus>(GameStatus.Playing);
+
+  useEffect(() => {
+    setWordOfTheDay("break");
+  });
   return (
     <div>
-      <RowCompleted word="sabio" solution="sabia" />
-      <RowCompleted word="sabio" solution="break" />
+      <RowCompleted word="sabio" solution={wordOfTheDay} />
+      <RowCompleted word="sabio" solution={wordOfTheDay} />
       <RowCurrent word="sabo" />
       <RowEmpty />
       <RowEmpty />
