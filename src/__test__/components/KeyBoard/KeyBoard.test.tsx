d7fb38e361d99d4d258717731fbd129a8fa5b661
Marlon Gómez <marlonjulian15@gmail.com>
2022-01-25 12:41:34 -0500

    Create tests for KeyBoard component

---
+++ src/__test__/components/KeyBoard/KeyBoard.test.tsx
@@ -0,0 +1,24 @@
+import { mount } from "enzyme";
+import { KeyBoard } from "../../../components/KeyBoard/KeyBoard";
+import { keys } from "../../../constants";
+
+describe("Test for <KeyBoard />", () => {
+  const onKeyPressed = jest.fn();
+  const keyboard = mount(<KeyBoard keys={keys} onKeyPressed={onKeyPressed} />);
+
+  test("Render KeyBoard component", () => {
+    expect(keyboard.length).toEqual(1);
+  });
+
+  test("KeyBoard component should have .keyboardContainer class", () => {
+    expect(keyboard.find(".keyboardContainer").length).toEqual(1);
+  });
+
+  test("KeyBoard component should have correct number of children (keys)", () => {
+    expect(keyboard.find("button").length).toEqual(28);
+  });
+
+  test("KeyBoard component with Snapshot", () => {
+    expect(keyboard).toMatchSnapshot();
+  });
+});
