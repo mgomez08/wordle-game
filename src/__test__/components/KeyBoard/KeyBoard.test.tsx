52dcc7e82d5f1e18600d2753af7fe8bcbcf24a3a
Marlon Gómez <marlonjulian15@gmail.com>
2022-01-27 14:03:13 -0500

    Fix test in Keyboard

--- src/__test__/components/KeyBoard/KeyBoard.test.tsx
+++ src/__test__/components/KeyBoard/KeyBoard.test.tsx
@@ -15,7 +15,7 @@ describe("Test for <KeyBoard />", () => {
   });
 
   test("KeyBoard component should have correct number of children (keys)", () => {
-    expect(keyboard.find("button").length).toEqual(28);
+    expect(keyboard.find("button").length).toEqual(29);
   });
 
   test("Simulate click on first key", () => {
