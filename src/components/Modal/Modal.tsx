fe43fd65b2b3b46d14aa688c6fc5051833e013c1
Marlon Gómez <marlonjulian15@gmail.com>
2022-01-28 12:13:22 -0500

    Add button close and social share

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -11,11 +11,29 @@ export const Modal = ({ type, completedWords, solution }: ModalProps) => {
   return (
     <div className={styles.modalViewContainer}>
       <div className={styles.modalContainer}>
-        <h2>You {type === "won" ? "Won!" : "Lost :("}</h2>
-        <div className={styles.puzzle}>
-          {completedWords.map((word, index) => (
-            <Square key={index} word={word} solution={solution} />
-          ))}
+        <div className={styles.modalHeader}>
+          <button>
+            <i className={"far fa-times-circle " + styles.btnClose}></i>
+          </button>
+        </div>
+        <div className={styles.modalContent}>
+          <h2>You {type === "won" ? "Won!" : "Lost :("}</h2>
+          <div className={styles.puzzle}>
+            {completedWords.map((word, index) => (
+              <Square key={index} word={word} solution={solution} />
+            ))}
+          </div>
+          <div className={styles.socialShareContainer}>
+            <h3>Share your score!</h3>
+            <div className={styles.social}>
+              <a
+                className={`fab fa-twitter-square ${styles.btnSocial} ${styles.twitterLogo}`}
+              ></a>
+              <a
+                className={`fab fa-whatsapp-square ${styles.btnSocial} ${styles.whatsappLogo}`}
+              ></a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
