b67bc1189fecc16717ab3cb8e3453ef3bc078a50
Marlon Gómez <marlonjulian15@gmail.com>
2022-01-31 12:56:58 -0500

    Use GameProvider in Root App

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import WordleApp from "./WordleApp";
+import { GameProvider } from "./context/GameContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <WordleApp />
+    <GameProvider>
+      <WordleApp />
+    </GameProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
