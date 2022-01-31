342278d474057e36c475405a4c6cd41fd6d776be
Marlon Gómez <marlonjulian15@gmail.com>
2022-01-31 12:57:18 -0500

    Use GameContext in WordleApp

--- src/WordleApp.tsx
+++ src/WordleApp.tsx
@@ -1,28 +1,28 @@
-import { useEffect, useState } from "react";
+import { useContext } from "react";
 import { RowCompleted } from "./components/Rows/RowCompleted";
 import { RowCurrent } from "./components/Rows/RowCurrent";
 import { RowEmpty } from "./components/Rows/RowEmpty";
 import { useWindow } from "./hooks/useWindow";
 import { GameStatus } from "./types/types";
 import { keys } from "./constants";
-import { getWordOfTheDay, isValidWord } from "./services/words";
 
 import style from "./wordleapp.module.scss";
 import { KeyBoard } from "./components/KeyBoard/KeyBoard";
 import { Modal } from "./components/Modal/Modal";
 import { Header } from "./components/Header/Header";
+import { GameContext } from "./context/GameContext";
 
 export default function WordleApp() {
-  const [wordOfTheDay, setWordOfTheDay] = useState<string>("");
-  const [turn, setTurn] = useState<number>(1);
-  const [currentWord, setCurrentWord] = useState<string>("");
-  const [completedWords, setCompletedWords] = useState<string[]>([]);
-  const [gameStatus, setGameStatus] = useState<GameStatus>(GameStatus.Playing);
-  const [openModal, setOpenModal] = useState<boolean>(false);
-
-  useEffect(() => {
-    setWordOfTheDay(getWordOfTheDay());
-  }, []);
+  const {
+    wordOfTheDay,
+    turn,
+    currentWord,
+    completedWords,
+    gameStatus,
+    openModal,
+    setOpenModal,
+    onKeyPressed,
+  } = useContext(GameContext);
 
   const handleKeyDown = (event: KeyboardEvent) => {
     const key = event.key.toUpperCase();
@@ -30,63 +30,6 @@ export default function WordleApp() {
   };
 
   useWindow("keydown", handleKeyDown);
-
-  const onInput = (letter: string) => {
-    const newWord = currentWord + letter;
-    setCurrentWord(newWord);
-  };
-
-  const onKeyPressed = (key: string) => {
-    if (gameStatus !== GameStatus.Playing) return;
-
-    if (key === "BACKSPACE" && currentWord.length > 0) {
-      onDelete();
-      return;
-    }
-
-    if (key === "ENTER" && currentWord.length === 5 && turn <= 6) {
-      onEnter();
-      return;
-    }
-
-    if (currentWord.length >= 5) return;
-
-    if (keys.includes(key)) {
-      onInput(key);
-      return;
-    }
-  };
-  const onEnter = async () => {
-    //User has completed a word
-    if (currentWord === wordOfTheDay) {
-      setCompletedWords([...completedWords, currentWord]);
-      setGameStatus(GameStatus.Won);
-      setOpenModal(true);
-      return;
-    }
-    //User has lost
-    if (turn === 6) {
-      setCompletedWords([...completedWords, currentWord]);
-      setGameStatus(GameStatus.Lost);
-      setOpenModal(true);
-      return;
-    }
-    const validWord = await isValidWord(currentWord);
-    if (currentWord.length === 5 && !validWord) {
-      alert("Word is no valid");
-      return;
-    }
-
-    //Valid word, but not the word of the day
-    setCompletedWords([...completedWords, currentWord]);
-    setTurn(turn + 1);
-    setCurrentWord("");
-  };
-
-  const onDelete = () => {
-    const newWord = currentWord.slice(0, -1);
-    setCurrentWord(newWord);
-  };
 
   return (
     <>
